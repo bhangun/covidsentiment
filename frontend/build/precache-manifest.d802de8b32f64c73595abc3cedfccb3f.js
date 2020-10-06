64f356692d659f595aa86c5003e6ac6337499801
bhangun <bhangunh@gmail.com>
2020-10-06 20:25:32 +0700

    just backend docker

---
+++ frontend/build/precache-manifest.d802de8b32f64c73595abc3cedfccb3f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "719e160d1c769baa9e8e47f8612c42b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c90566a0f98ec4d3dd00",
+    "url": "/static/css/main.10fadeae.chunk.css"
+  },
+  {
+    "revision": "0d600f3f9a9e69c98e23",
+    "url": "/static/js/2.f051b0ad.chunk.js"
+  },
+  {
+    "revision": "bcfb22a347e5cdb8717701c69bea4918",
+    "url": "/static/js/2.f051b0ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c90566a0f98ec4d3dd00",
+    "url": "/static/js/main.5c7fcec5.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "982ecb56af72d398d3459e3a81ae083a",
+    "url": "/static/media/virus.982ecb56.png"
+  }
+]);
